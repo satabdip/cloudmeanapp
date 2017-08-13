ed52589d822cb496d8d4f71c49a4aed73286e6d8
satabdip <satabeli08@gmail.com>
2017-08-13 13:38:57 +0530

    First commit

--- config/database.js
+++ config/database.js
@@ -1,5 +1,6 @@
 module.exports = {
 
 	// the database url to connect!
-	url: 'mongodb://todoAholic:scotchio@ec2-52-0-14-185.compute-1.amazonaws.com:27017/dummyDB'
+	//url: 'mongodb://todoAholic:scotchio@ec2-52-0-14-185.compute-1.amazonaws.com:27017/dummyDB'
+	url: 'mongodb://satabdi:123@ec2-54-145-86-20.compute-1.amazonaws.com:27017/dummyDB'
 }
